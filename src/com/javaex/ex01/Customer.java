d3fc60d3c675544b524c375beac6f07e4d43bfb5
YuYeaJin <yyj9290@naver.com>
2024-11-08 18:12:55 +0900

    01번

--- src/com/javaex/ex01/Customer.java
+++ src/com/javaex/ex01/Customer.java
@@ -1,7 +1,21 @@
 package com.javaex.ex01;
 
 public class Customer extends Person{
+	private int cNo;
+	private int point;
 	
-
+	public Customer() {
+		super();
+	}
 	
+	public Customer(String name, String hp, int cNo, int point) {
+		super(name, hp);
+		this.cNo = cNo;
+		this.point = point;
+	}
+	
+	@Override
+	public void showInfo() {
+		System.out.println("#이름: " + name + "#핸드폰: " + hp + "#고객번호: " + cNo + "#포인트점수: " + point);
+	}
 }
